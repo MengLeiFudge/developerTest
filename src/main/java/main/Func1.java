cd9a717c241845ccc8db63f62fbc8b270f7f274c
miniMengLei <605738729@qq.com>
2020-11-27 19:25:13 +0800

    first

--- src/main/java/main/Func1.java
+++ src/main/java/main/Func1.java
@@ -1,13 +1,28 @@
 package main;
 
+import org.apache.commons.io.FileUtils;
+
+import java.io.File;
+import java.io.IOException;
+
 public class Func1 {
 
-    Func1(){}
-
-
-    public void process(){
-        System.out.println(123);
+    Func1() {
     }
 
+    public void process() {
+        System.out.println(123);
+        File f = new File("files/1.txt");
+        try {
+            FileUtils.forceMkdirParent(f);
+            if (!f.createNewFile()) {
+                System.out.println("创建失败！");
+            } else {
+                System.out.println("创建成功！");
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
 }
